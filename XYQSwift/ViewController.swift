2dd7de4b677aec0e407a9902ff5003da9517e20d
Alex <850946554@qq.com>
2016-05-07 15:59:04 +0800

    test commit

---
+++ XYQSwift/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  XYQSwift
+//
+//  Created by AlexZHOU on 7/5/16.
+//  Copyright © 2016年 AlexZHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func close(segue:UIStoryboardSegue){
+        print("CryViewControllerClose")
+    }
+    
+}
+
