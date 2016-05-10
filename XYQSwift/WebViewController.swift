5454eb39999ad5901f696fdd161cb41a1d0f0b9b
Alex <850946554@qq.com>
2016-05-10 23:14:11 +0800

    aa

---
+++ XYQSwift/WebViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WebViewController.swift
+//  XYQSwift
+//
+//  Created by AlexZHOU on 10/5/16.
+//  Copyright © 2016年 AlexZHOU. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let  webView:UIWebView = UIWebView();
+        webView.frame = self.view.bounds
+        self.view.addSubview(webView)
+        
+        let url:NSURL;
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
